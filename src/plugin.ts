a1c4433e28065ec79bda63262830344835cbc386
ageuxo <ageuxo@gmail.com>
2026-02-15 14:55:53 +0100

    Simple base

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,53 +1,18 @@
-import createExtendedCodec from "./codec";
 
 const deletables: Deletable[] = [];
 
-class EventCallback implements Deletable {
-    event: EventName;
-    cb: any;
-
-    constructor(event: EventName, cb: any) {
-        this.event = event;
-        this.cb = cb;
-
-        Blockbench.on(event, cb);
-    }
-
-    delete(): void {
-        Blockbench.removeListener(this.event, this.cb)
-    }
-}
-
 const plugin: PluginOptions = {
-    title: 'Dynamo Library',
+    title: 'Animation Test',
     author: 'ageuxo',
     icon: 'icon.png',
     version: '1.0.0',
-    description: 'Adds a custom model format for animating models in Minecraft',
+    description: 'Animation Test',
     variant: 'both',
     min_version: '5.0.0',
-    has_changelog: true,
+    has_changelog: false,
     onload() {
-
-        console.log("Dynamo Library loaded");
-        var extendedCodec = createExtendedCodec();
-        var extendedFormat = createExtendedModelFormat(extendedCodec);
-        extendedCodec.format = extendedFormat;
-
-        const exportAction = new Action('export_dynamo_model', {
-            name: 'Export Dynamo Model',
-            icon: 'icon.png',
-            category: 'file',
-            condition: () => Project.format.id == extendedFormat.id,
-            click: () => {
-                extendedCodec.export();
-            }
-        });
-        extendedCodec.export_action = exportAction;
-
-        MenuBar.addAction(exportAction, 'file.export');
-
-        deletables.push(extendedFormat, extendedCodec, exportAction);
+        
+        deletables.push();
 
     },
     onunload() {
@@ -59,41 +24,7 @@ const plugin: PluginOptions = {
     }
 }
 
-BBPlugin.register('dynamo_lib', plugin)
+BBPlugin.register('animation_test', plugin)
 
-function createExtendedModelFormat(extendedCodec: Codec): ModelFormat {
-    const extendedFormat = new ModelFormat('dynamo_extended', {
-        id: 'dynamo_extended',
-        name: 'Extended DynamoLib model',
-        icon: 'change_history',
-        category: 'minecraft',
-        target: 'Minecraft: Java Edition',
-        render_sides: 'front',
-        model_identifier: false,
-        parent_model_id: true,
-        vertex_color_ambient_occlusion: true,
-        rotate_cubes: true,
-        rotation_limit: true,
-        rotation_snap: false,
-        optional_box_uv: true,
-        uv_rotation: true,
-        java_cube_shading_properties: true,
-        java_face_properties: true,
-        cullfaces: true,
-        animated_textures: true,
-        select_texture_for_particles: true,
-        texture_mcmeta: true,
-        display_mode: true,
-        texture_folder: true,
-        pbr: true,
-        animation_mode: true,
-        animation_files: true,
-        animation_grouping: "disabled"
-    });
-
-    extendedFormat.codec = extendedCodec;
-
-    return extendedFormat;
-}
 
 
